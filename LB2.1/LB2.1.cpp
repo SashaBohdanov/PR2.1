25dc56ebbe418dd02a045fbabdc23a21801a406c
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-09-28 15:37:28 +0300

    first commit

---
+++ LB2.1/LB2.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double sin; // вхідний параметр (значення sin)
+    double cos; // вхідний параметр (значення cos)
+    double z1;       // результат обчислення 1-го виразу
+    double z2;       // результат обчислення 2-го виразу
+
+    cout << "sin = "; cin >> sin;
+    cout << "cos = "; cin >> cos;
+
+    // Виправлення формули для z1
+    z1 = (pow(sin, 2) + pow(sin, 5) - pow(sin, 3)) / (cos * sin) + 1 - (2 * pow(sin, 2) * 2 * sin);
+
+    // Виправлення формули для z2
+    z2 = 2 * sin;
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
